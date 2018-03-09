3519c6f6b0f8a442f9ab939f93d948bf1c56576d
rayGreycloud <rayGreycloud@gmail.com>
2018-03-09 13:59:17 -0800

    Add test of sell article function

--- test/ChainList.test.js
+++ test/ChainList.test.js
@@ -19,5 +19,23 @@ contract('ChainList', accounts => {
       });
   });
   
-
+  it("should sell an article", () => {
+    return ChainList.deployed()
+      .then(instance => {
+        chainListInstance = instance;
+        chainListInstance.sellArticle(
+          articleName,
+          articleDescription,
+          web3.toWei(articlePrice, "ether"),
+          { from: seller }
+        )
+      })
+      .then(() => chainListInstance.getArticle())
+      .then(data => {
+        assert.equal(data[0], seller, `seller must be ${seller}`);
+        assert.equal(data[1], articleName, `article name must be ${articleName}`);
+        assert.equal(data[2], articleDescription, `article description must be ${articleDescription}`);
+        assert.equal(data[3].toNumber(), web3.toWei(articlePrice, "ether"), `article price must be ${articlePrice}`);
+      });
+  });
 });
