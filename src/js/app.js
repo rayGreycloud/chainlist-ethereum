096a23331a127c770a3f735c242620291c195384
rayGreycloud <rayGreycloud@gmail.com>
2018-03-09 15:50:29 -0800

    On init, inject article data into template and insert in target div

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,33 @@
 App = {
-     web3Provider: null,
-     contracts: {},
+  web3Provider: null,
+  contracts: {},
 
-     init: function() {
-          /*
-           * Replace me...
-           */
+  init: function() {
+    let articlesRow = $('#articlesRow');
+    let articleTemplate = $('#articleTemplate');
+    
+    articleTemplate.find('.panel-title').text('Article 1');
+    articleTemplate.find('.article-description').text('Description of Article 1');
+    articleTemplate.find('.article-price').text('2.12');
+    articleTemplate.find('.article-seller').text('0x12345678901234567890');
 
-          return App.initWeb3();
-     },
+    articlesRow.append(articleTemplate.html());
+    
+    return App.initWeb3();
+  },
 
-     initWeb3: function() {
-          /*
-           * Replace me...
-           */
+  initWeb3: function() {
 
-          return App.initContract();
-     },
+    return App.initContract();
+  },
 
-     initContract: function() {
-          /*
-           * Replace me...
-           */
-     },
+  initContract: function() {
+
+  },
 };
 
 $(function() {
-     $(window).load(function() {
-          App.init();
-     });
+  $(window).load(function() {
+    App.init();
+  });
 });
