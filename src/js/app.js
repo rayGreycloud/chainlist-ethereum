6fc749d1c21f26adf9368bc31f5faf2109557705
rayGreycloud <rayGreycloud@gmail.com>
2018-03-09 16:38:18 -0800

    Initialize web3 and display account info

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,42 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: 0x0,
 
   init: function() {
-    let articlesRow = $('#articlesRow');
-    let articleTemplate = $('#articleTemplate');
-    
-    articleTemplate.find('.panel-title').text('Article 1');
-    articleTemplate.find('.article-description').text('Description of Article 1');
-    articleTemplate.find('.article-price').text('2.12');
-    articleTemplate.find('.article-seller').text('0x12345678901234567890');
-
-    articlesRow.append(articleTemplate.html());
-    
     return App.initWeb3();
   },
 
   initWeb3: function() {
-
+    // initialize web3 
+    if (typeof web3 !== 'undefined') {
+      // reuse provider from Metamask web3 object
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // create a new provider and plug into local node
+      App.web3Provider = new Web3.providers.HttpProvider('htpp://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
+    
+    // Display account info
+    App.displayAccountInfo();
+    
     return App.initContract();
   },
 
+  displayAccountInfo: function() {
+    web3.eth.getCoinbase(function (err, account) {
+      if (err === null) {
+        App.account = account;
+        $('#account').text(account);
+        
+        web3.eth.getBalance(account, function (err, balance) {
+          $('#accountBalance').text(web3.fromWei(balance, 'ether') + ' ETH');
+        });
+      }
+    }); 
+  },
+  
   initContract: function() {
 
   },
