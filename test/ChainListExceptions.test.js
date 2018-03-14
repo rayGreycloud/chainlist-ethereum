58ecb3ff3fff46b2689c95b3007471ef0aa69fbb
rayGreycloud <rayGreycloud@gmail.com>
2018-03-14 10:54:56 -0700

    Test contract exceptions

---
+++ test/ChainListExceptions.test.js
@@ -0,0 +1,36 @@
+const ChainList = artifacts.require("./ChainList.sol");
+
+// test suite 
+contract('ChainList', accounts => {
+  let chainListInstance;
+  const seller = accounts[1];
+  const buyer = accounts[2];
+  const articleName = "Wacky Widget";
+  const articleDescription = "Widget that is wacky"; 
+  const articlePrice = 10;
+  
+  // No article for sale 
+  it("should throw exception when buy article is attempted and no article is for sale", () => {
+    return ChainList.deployed()
+      .then(instance => {
+        chainListInstance = instance;
+        return chainListInstance.buyArticle({ 
+          from: buyer, 
+          value: web3.toWei(articlePrice, "ether") 
+        });
+      })
+      // If here then test failed
+      .then(assert.fail)
+      // Expected error passes test
+      .catch(error => assert(true))
+      // Check article unchanged
+      .then(() => chainListInstance.getArticle())
+      .then(data => {
+        assert.equal(data[0], 0x0, "seller must be empty");
+        assert.equal(data[1], 0x0, "buyer must be empty");
+        assert.equal(data[2], "", "article name must be empty");
+        assert.equal(data[3], "", "article description must be empty");
+        assert.equal(data[4].toNumber(), 0, "article price must be empty");
+      });          
+  });
+});
