6fde8d15b7a54a794872c272a2720122d5e9dd3b
rayGreycloud <rayGreycloud@gmail.com>
2018-03-14 19:12:17 -0700

    Correct test of seller buying exception

--- test/ChainListExceptions.test.js
+++ test/ChainListExceptions.test.js
@@ -34,28 +34,28 @@ contract('ChainList', accounts => {
       });          
   });
   
-  // Seller attempts to buy  
+  // Seller attempts to buy 
   it("should throw exception when seller attempts to buy own article", () => {
     return ChainList.deployed()
       .then(instance => {
         chainListInstance = instance;
-        return chainListInstance.buyArticle({ 
-          from: seller, 
-          value: web3.toWei(articlePrice, "ether") 
-        });
+        return chainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), { from: seller});
       })
-      // If here then test failed
+      .then(receipt => {
+        return chainListInstance.buyArticle({from: seller, value: web3.toWei(articlePrice, "ether")});
+      })
       .then(assert.fail)
-      // Expected error passes test
-      .catch(error => assert(true))
-      // Check article unchanged
+      .catch(error => {
+        assert(true);
+      })
       .then(() => chainListInstance.getArticle())
       .then(data => {
-        assert.equal(data[0], 0x0, "seller must be empty");
-        assert.equal(data[1], 0x0, "buyer must be empty");
-        assert.equal(data[2], "", "article name must be empty");
-        assert.equal(data[3], "", "article description must be empty");
-        assert.equal(data[4].toNumber(), 0, "article price must be empty");
-      });          
-  });
+        assert.equal(data[0], seller, `seller must be ${seller}`);
+        assert.equal(data[1], 0x0, `buyer must be empty`);
+        assert.equal(data[2], articleName, `article name must be ${articleName}`);
+        assert.equal(data[3], articleDescription, `article description must be ${articleDescription}`);
+        assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), `article price must be ${web3.toWei(articlePrice, "ether")}`);
+    });
+  });  
+   
 });
