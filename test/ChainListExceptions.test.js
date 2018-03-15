02f4f400ca671b0c8b2bb55d08d81ce352e6580a
rayGreycloud <rayGreycloud@gmail.com>
2018-03-14 19:23:45 -0700

    Test for exception if buyer sends wrong value

--- test/ChainListExceptions.test.js
+++ test/ChainListExceptions.test.js
@@ -57,5 +57,29 @@ contract('ChainList', accounts => {
         assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), `article price must be ${web3.toWei(articlePrice, "ether")}`);
     });
   });  
-   
+  
+  // Buyer sends wrong price 
+  it("should throw exception if buyer does not send correct value", () => {
+    return ChainList.deployed()
+      .then(instance => {
+        chainListInstance = instance;
+        return chainListInstance.buyArticle({ 
+          from: buyer, 
+          value: web3.toWei(articlePrice - 1, "ether") 
+        });
+      })
+      .then(assert.fail)
+      .catch(error => {
+        assert(true);
+      })
+      .then(() => chainListInstance.getArticle())
+      .then(data => {
+        assert.equal(data[0], seller, `seller must be ${seller}`);
+        assert.equal(data[1], 0x0, `buyer must be empty`);
+        assert.equal(data[2], articleName, `article name must be ${articleName}`);
+        assert.equal(data[3], articleDescription, `article description must be ${articleDescription}`);
+        assert.equal(data[4].toNumber(), web3.toWei(articlePrice, "ether"), `article price must be ${web3.toWei(articlePrice, "ether")}`);
+    });
+  });  
+
 });
